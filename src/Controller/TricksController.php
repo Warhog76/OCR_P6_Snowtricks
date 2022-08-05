4e1d305a9893fb23a2609811f0a49270d5af3710
warhog76 <warhog76@free.fr>
2022-08-05 16:05:52 +0200

    Forms mod

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -10,6 +10,7 @@ use App\Form\TricksFormType;
 use App\Repository\CommentRepository;
 use App\Repository\MediaRepository;
 use App\Repository\TricksRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -29,12 +30,12 @@ class TricksController extends AbstractController
     }
 
     #[Route("", name: 'home')]
-    public function home(TricksRepository $trickRepository): Response
+    public function home(TricksRepository $trickRepo, MediaRepository $mediaRepo): Response
     {
-        $tricks = $trickRepository->findAll();
+        $tricks = $trickRepo->findAll();
 
         return $this->render('home.html.twig',  [
-            'tricks' => $tricks,
+            'tricks' => $tricks
         ]);
     }
 
@@ -88,7 +89,7 @@ class TricksController extends AbstractController
     }
 
     #[Route('/tricks/{id}', name: 'show')]
-    public function show($id, Request $request, CommentRepository $commentRepo, MediaRepository $mediaRepo): Response
+    public function show($id, Request $request, UserRepository $userRepo, CommentRepository $commentRepo, MediaRepository $mediaRepo): Response
     {
         $repo = $this->entityManager->getRepository(Tricks::class);
         $tricks = $repo->find($id);
@@ -107,15 +108,16 @@ class TricksController extends AbstractController
 
             $this->entityManager->persist($comment);
             $this->entityManager->flush();
-
-
         }
+
         return $this->render('tricks/show.html.twig', [
             'tricks' => $tricks,
-            'medias' => $mediaRepo->findBy(
-                ['tricks' => $tricks->getId()]),
-            'comments' => $commentRepo->findBy(
-                ['tricks' => $tricks->getId()]),
+            'user' => $userRepo->find([
+                'id' => $tricks->getuser()]),
+            'medias' => $mediaRepo->findby([
+                'tricks' => $tricks->getId()]),
+            'comments' => $commentRepo->findBy([
+                'tricks' => $tricks->getId()]),
             'comment_form' => $form->createView()
             ]
         );
@@ -125,14 +127,14 @@ class TricksController extends AbstractController
     #[Route('/tricks/edit/{id}', name: 'tricks_modify')]
     public function modify($id): Response
     {
-        $repoTricks = $this->entityManager->getRepository(Tricks::class);
-        $repoMedia = $this->entityManager->getRepository(Media::class);
-        $tricks = $repoTricks->find($id);
-        $media = $repoMedia->find($id);
+        $tricksRepo = $this->entityManager->getRepository(Tricks::class);
+        $mediaRepo = $this->entityManager->getRepository(Media::class);
+        $tricks = $tricksRepo->find($id);
 
         return $this->render('tricks/modify.html.twig', [
                 'tricks' => $tricks,
-                'medias' => $media
+                'medias' => $mediaRepo->findby([
+                    'tricks' => $tricks->getId()]),
             ]
         );
     }
