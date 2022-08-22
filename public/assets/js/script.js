a9b6be1c090c5507db7761ad4e361ca0238ce667
warhog76 <warhog76@free.fr>
2022-08-22 22:18:05 +0200

    templates under construction

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,19 +1,10 @@
-//Get the button:
-mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+    // Get the modal
+    modal = document.getElementById('id01');
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target === modal) {
+    modal.style.display = "none";
 }
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
