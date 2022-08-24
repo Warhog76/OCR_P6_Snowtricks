00271ebb0340827c7a31fce4555433dc64ab05e3
warhog76 <warhog76@free.fr>
2022-08-24 18:48:17 +0200

    templates finishing

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,20 +9,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
-    {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
-    }
-
-    #[Route('/user/reset', name: 'reset')]
-    public function resetPassword(): Response
-    {
-        return $this->render('user/reset.html.twig',
-        );
-    }
 
     #[Route('/user/profile', name: 'profile')]
     public function profile(): Response
